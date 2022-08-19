bd4c878aced7cf3e634f7ada57b790045bb2d8cc
DisuToyin <disujt@gmail.com>
2022-08-19 20:30:21 +0100

    merge list

--- leetcode/mergeTwoList.js
+++ leetcode/mergeTwoList.js
@@ -4,4 +4,24 @@
 
 // Return the head of the merged linked list.
 
-const mergeSortedLinkedList = (l1, l2) => {};
+const mergeSortedLinkedList = (l1, l2) => {
+  let curr = new ListNode();
+  let dummy = curr;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  if (l1) {
+    curr.next = l1;
+  }
+  if (l2) {
+    curr.next = l2;
+  }
+  return dummy.next;
+};
