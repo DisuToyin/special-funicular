2e4b77f47f3ff9990a24a45e46ec9156d923e48e
DisuToyin <disujt@gmail.com>
2022-08-20 09:21:47 +0100

    merge sort better understanding

--- leetcode/mergeTwoList.js
+++ leetcode/mergeTwoList.js
@@ -4,24 +4,51 @@
 
 // Return the head of the merged linked list.
 
+const printLinkedList = (head) => {
+  let current = head;
+  while (current !== null) {
+    console.log(current.val);
+    current = current.next;
+  }
+};
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// l1;
+const a = new Node(1);
+const b = new Node(4);
+
+a.next = b;
+
+// l2;
+const w = new Node(-1);
+const x = new Node(2);
+const y = new Node(3);
+
+w.next = x;
+x.next = y;
+
 const mergeSortedLinkedList = (l1, l2) => {
-  let curr = new ListNode();
-  let dummy = curr;
+  let current = new Node();
+  let dummy = current;
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      curr.next = l1;
+      current.next = l1;
       l1 = l1.next;
     } else {
-      curr.next = l2;
+      current.next = l2;
       l2 = l2.next;
     }
-    curr = curr.next;
+    current = current.next;
   }
-  if (l1) {
-    curr.next = l1;
-  }
-  if (l2) {
-    curr.next = l2;
-  }
+  if (l1) current.next = l1;
+  if (l2) current.next = l2;
+  printLinkedList(dummy.next);
   return dummy.next;
 };
+
+console.log(mergeSortedLinkedList(a, w));
